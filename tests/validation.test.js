7b3cefc9def8bc9379e65474bff8fbdbe29720d1
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-08-08 19:25:39 +0700

    tried validation with joi

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -1,31 +1,43 @@
 import Joi from "joi";
 
-describe('Joi', function () {
-    it('should can create schema', function () {
-        const schema = Joi.string().min(3).max(100).required();
+describe("Validation", function () {
+  it("should create and validate schema", function () {
+    const schema = Joi.string().min(3).max(100).required();
 
-        const result = schema.validate("eko");
-        if (result.error) {
-            console.info(result.error);
-        }
-    });
+    const result = schema.validate("wafa");
+    if (result.error) {
+      console.info(result.error);
+    }
 
-    it('should can validate basic data type', function () {
-        const usernameSchema = Joi.string().email().required();
-        const isAdminSchema = Joi.boolean().required();
-        const priceSchema = Joi.number().required().min(1000).max(1000000);
+    expect(result.value).toBe("wafa");
+  });
 
-        const resultUsername = usernameSchema.validate("eko@gmail.com");
-        console.info(resultUsername);
+  it("should throw errow if the value does not meet requirements", function () {
+    const schema = Joi.string().min(3).max(100).required();
 
-        const resultIsAdmin = isAdminSchema.validate("true");
-        console.info(resultIsAdmin);
+    const result = schema.validate("w");
+    if (result.error) {
+      console.info(result.error);
+    }
 
-        console.info(typeof "true");
-        console.info(typeof resultIsAdmin.value);
-        console.info(typeof resultIsAdmin.error);
+    expect(result.error).toBeDefined();
+  });
 
-        const resultPrice = priceSchema.validate("10000");
-        console.info(resultPrice);
-    });
+  it("should validate basic data type", function () {
+    const usernameSchema = Joi.string().email().required();
+    const isAdminSchema = Joi.boolean().required();
+    const priceSchema = Joi.number().required().min(1000).max(1000000);
+
+    const resultUsername = usernameSchema.validate("eko@gmail.com");
+    console.info(resultUsername);
+
+    const resultIsAdmin = isAdminSchema.validate("true");
+    console.info(resultIsAdmin);
+    console.info(resultIsAdmin.error);
+
+    const resultPrice = priceSchema.validate("10000");
+    console.info(resultPrice);
+
+    expect(resultUsername.value).toBe("eko@gmail.com");
+  });
 });
